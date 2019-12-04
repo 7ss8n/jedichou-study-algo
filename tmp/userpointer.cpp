fae7186c06765213889a87fbe7d9e4bcadb84648
jedichou <jedigit@163.com>
2019-12-04 19:34:24 +0800

    修订userpointer.cpp内注释

--- tmp/userpointer.cpp
+++ tmp/userpointer.cpp
@@ -1,23 +1,37 @@
+// File name: userpointer.cpp
+// Description: 简单演示指针绑定前后的内存地址变化
+
 #include <iostream>
+
 using namespace std;
+
 int main() {
+	
+	// 赋值
 	int i;
 	int *p;
 
-	// process-1
+	// 1. 赋值前地址和值
+	cout << "赋值前" << endl;
 	cout << "i addr: " << &i << endl;
 	cout << "p value: " << p << endl;
 	cout << "p addr: " << &p << endl;
+	cout << endl;
 
+	// 2. i赋值后的地址和值
 	i = 3;
-	cout << "after i = 3" << endl;
+	cout << "赋值后 i = 3" << endl;
 	cout << "i addr: " << &i << endl;
+	cout << endl;
 	
+	// 3. p绑定为i的地址后
 	p = &i;
-	cout << "after p = &i" << endl;
+	cout << "p绑定为i的地址后 p = &i" << endl;
 	cout << "i addr: " << &i << endl;
 	cout << "p value: " << p << endl;
 	cout << "p addr: " << &p << endl;
+	cout << "*p: " << *p << endl;
+	cout << endl;
 
 	return 0;
 }
