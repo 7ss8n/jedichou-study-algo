0b612563880b49e6aa1b3b853afc7f7e8793f987
jedichou <jedigit@163.com>
2019-12-05 15:19:15 +0800

    调整代码格式

--- tmp/360-problem-171021-1527.c
+++ tmp/360-problem-171021-1527.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-float p0=1000,r1=0.0036,r2=0.0225,r3=0.0198,p1,p2,p3;
-p1=p0*(1+r1);
-p2=p0*(1+r2);
-p3=p0*(1+r3/2)*(1+r3/2);
-printf("p1=%d\np2=%d\np3=%d\n",p1,p2,p3);
-return 0;
+    float p0=1000,r1=0.0036,r2=0.0225,r3=0.0198,p1,p2,p3;
+    p1=p0*(1+r1);
+    p2=p0*(1+r2);
+    p3=p0*(1+r3/2)*(1+r3/2);
+    printf("p1=%d\np2=%d\np3=%d\n",p1,p2,p3);
+    return 0;
 }
